7d62008faa4308299736e54e9bfac9a7eaccd52a
Miguel Zufelatto <zufelatto@gmail.com>
2024-12-26 15:57:40 -0300

    Update script.js

--- desafios/d009/script.js
+++ desafios/d009/script.js
@@ -6,26 +6,33 @@ function reajuste() {
     return;
   }
   let sal = window.prompt(`Qual é o salário de ${nome}`);
-  sal.replace(",", ".");
-  if ((sal = "" | (sal <= 0))) {
+  sal = sal.replace(",", ".");
+  let salN = Number(sal);
+  if (isNaN(salN) | (sal <= 0)) {
     window.alert("Por favor insira um salário válido");
     return;
   }
-  let salN = Number(sal);
-
   let porcento = window.prompt(
     `O salário de ${nome} vai ser reajustado em qual porcentagem?`
   );
-  if ((porcento = "" | (porcento <= 0))) {
+  porcento = porcento.replace(",", ".");
+  let porcentoN = Number(porcento);
+  if (isNaN(porcentoN) | (porcentoN <= 0)) {
     window.alert("Por favor insira um salário válido");
     return;
   }
-  let porcentoN = Number(porcento);
-
-  let porcentsal = salN / (porcentoN / 100);
+  let porcentsal = salN * (porcentoN / 100);
   let novosal = porcentsal + salN;
-  res.innerHTML = `<h2>${nome} recebeu um aumento salarial!</h2> 
-  <p>O salário atual era ${salN}</p> 
-  <p>Com um aumento de ${porcentoN}%, o salário vai aumentar R$ ${porcentsal} no próximo mês</p> 
-  <p>E a partir daí, ${nome} vai passar a ganhar R$ ${novosal}</p>`;
+  res.innerHTML = `<h2>${nome.toLocaleString(
+    "pt-BR"
+  )} recebeu um aumento salarial!</h2> 
+  <p>O salário atual era ${salN.toLocaleString("pt-BR")}</p> 
+  <p>Com um aumento de ${porcentoN.toLocaleString(
+    "pt-BR"
+  )}%, o salário vai aumentar R$ ${porcentsal.toLocaleString(
+    "pt-BR"
+  )} no próximo mês</p> 
+  <p>E a partir daí, ${nome.toLocaleString(
+    "pt-BR"
+  )} vai passar a ganhar R$ ${novosal.toLocaleString("pt-BR")}</p>`;
 }
