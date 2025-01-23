e483a8c1719e41557170b2dfa7405827b7d96778
Miguel Zufelatto <zufelatto@gmail.com>
2025-01-23 11:51:51 -0300

    debug e estilo

--- desafios/d011/script.js
+++ desafios/d011/script.js
@@ -1,13 +1,17 @@
 function bissexto() {
   const res = document.getElementById("res");
+  const a = window.prompt("Qual é o ano que voce quer verificar?");
+  const ano = Number(a);
 
-  if (res.innerHTML !== "") {
-    alert("Por favor limpe o resultado antes de inserir outro valor.");
+  if (isNaN(ano)) {
+    alert("Por favor digite um número válido");
     return;
   }
 
-  const a = window.prompt("Qual é o ano que voce quer verificar?");
-  const ano = Number(a);
+  if (res.style.display === "none" || res.style.display === "") {
+    res.style.display = "block"; // Mostra a div
+  }
+
   if (ano % 4 === 0 && (ano % 100 !== 0 || ano % 400 === 0)) {
     res.innerHTML = `<p>O ano de ${ano} <span class="bissexto">É BISSEXTO</span>✅</p>`;
   } else {
@@ -16,5 +20,10 @@ function bissexto() {
 }
 
 function limpa() {
-  res.innerHTML = "";
+  res.style.display = "none";
 }
+
+// if (res.innerHTML !== "") {
+//   alert("Por favor limpe o resultado antes de inserir outro valor.");
+//   return;
+// }
