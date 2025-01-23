71750c806f9eb3768ae0167b67a0828bf1dd046a
Miguel Zufelatto <zufelatto@gmail.com>
2025-01-22 23:09:26 -0300

    Update de estilo

--- desafios/d013/script.js
+++ desafios/d013/script.js
@@ -2,34 +2,41 @@ let res = document.getElementById("res");
 
 function situation() {
   let nome = window.prompt("Qual é o nome do aluno ?");
-  if (nome.trim() === "") {
+  nome = nome.trim();
+  if (nome === "") {
     alert("Por favor digite um nome.");
     return;
   }
-  const n1 = window.prompt(`Primeiro nota de ${nome}:`);
-  const n2 = window.prompt(`Segunda nota de ${nome}:`);
-  const media = (Number(n1) + Number(n2)) / 2;
+
+  const capitalize = (str) => `${str[0].toUpperCase()}${str.substr(1)}`;
+  nome = nome.split(" ").map(capitalize).join(" ");
+
+  let n1 = window.prompt(`Primeiro nota de ${nome}:`);
+  n1 = n1.replace(",", ".");
+  const nota1 = Number(n1);
+  if (isNaN(nota1)) {
+    alert("Por favor digite um número.");
+    return;
+  }
+
+  let n2 = window.prompt(`Segunda nota de ${nome}:`);
+  n2 = n2.replace(",", ".");
+  const nota2 = Number(n2);
+  if (isNaN(nota2)) {
+    alert("Por favor digite um número.");
+    return;
+  }
+
+  const media = (nota1 + nota2) / 2;
 
   if (media < 3) {
-    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${Number(
-      n1
-    )} e ${Number(
-      n2
-    )}, a média é ${media}</p><p>Com média abaixo de 3.0, o aluno está REPROVADO</p>`;
+    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${nota1} e ${nota2}, a média é ${media}</p><p>Com média abaixo de 3.0, o aluno está REPROVADO</p>`;
   }
   if (media >= 3 && media <= 6) {
-    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${Number(
-      n1
-    )} e ${Number(
-      n2
-    )}, a média é ${media}</p><p>Com média entre 3.0 e 6.0, o aluno está em RECUPERAÇÃO</p>`;
+    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${nota1} e ${nota2}, a média é ${media}</p><p>Com média entre 3.0 e 6.0, o aluno está em RECUPERAÇÃO</p>`;
   }
   if (media > 6) {
-    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${Number(
-      n1
-    )} e ${Number(
-      n2
-    )}, a média é ${media}</p><p>Com média acima de 6.0, o aluno está APROVADO</p>`;
+    res.innerHTML = `<h2>Analisando a situação de ${nome}</h2><p>Com as notas de ${nota1} e ${nota2}, a média é ${media}</p><p>Com média acima de 6.0, o aluno está APROVADO</p>`;
   }
 }
 
@@ -39,9 +46,6 @@ function limpa() {
     "<p>Clique no botão acima para informar as notas do aluno.</p>";
 }
 
-// CAPITALIZAR NOME
-// PERMITIR SOMENTE NUM NAS NOTAS
-// PERMITIR VIRGULA NA ENTRADA DE NOTAS
+// PARTÍCULAS DOS NOMES
 // FORMATAR PARA BR OS NUM
 // PERMITIR APENAS UM DEPOIS DA VIRGULA
-// ESTILIZAÇÃO
