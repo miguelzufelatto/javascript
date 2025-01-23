60183896a86a4a1ed7cd0e5432d8edc8daa57eb8
Miguel Zufelatto <zufelatto@gmail.com>
2025-01-23 13:41:09 -0300

    Update script.js

--- desafios/Aula 14ex/ex017/script.js
+++ desafios/Aula 14ex/ex017/script.js
@@ -1,31 +1,30 @@
 function tabuada() {
-    var n = document.getElementById('txtn')
-    var res = document.getElementById('seltab')
-    if (n.value.length == 0) {
-        window.alert('Por favor, digite um número!')
-        return
-    } else {
-        var v = Number(n.value)
-        var c = 1
+  var n = document.getElementById("txtn");
+  var res = document.getElementById("seltab");
+  if (n.value.length == 0) {
+    window.alert("Por favor, digite um número!");
+    return;
+  } else {
+    var v = Number(n.value);
+    var c = 1;
 
-        res.innerHTML = ''
+    res.innerHTML = "";
 
-        while (c <= 10) {
-            var item = document.createElement("option")
-            item.text = `${v} x ${c} = ${v*c}`
-            item.value = `tab${c}`
-            res.appendChild(item)
-            c++
-        }
+    while (c <= 10) {
+      var item = document.createElement("option");
+      item.text = `${v} x ${c} = ${v * c}`;
+      item.value = `tab${c}`;
+      res.appendChild(item);
+      c++;
     }
+  }
 }
 
-    /*
-    for (var c = 1; c <= 10; c++) {
-        var resultado = v * c
-        var item = document.createElement("option")
-        item.text = `${v} x ${c} = ${resultado}`
-        res.appendChild(item)
-    }
-}
-    */
+//     for (var c = 1; c <= 10; c++) {
+//         var resultado = v * c
+//         var item = document.createElement("option")
+//         item.text = `${v} x ${c} = ${resultado}`
+//         res.appendChild(item)
+//     }
+// }
+// Resolver problema de exibição dos resultados
